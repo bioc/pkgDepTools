fea3b6ddb676469758f2d4fc596fed57e35bb5a3
lshep <lori.shepherd@roswellpark.org>
2023-01-06 13:06:59 -0500

    Bioconductor Core Team package -- requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -5,3 +5,11 @@ globals <- new.env(parent=emptyenv(), hash=TRUE)
     globals$have_RCurl <- suppressWarnings(require("RCurl", quietly=TRUE))
 }
 .onLoad <- .First.lib
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.18")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
